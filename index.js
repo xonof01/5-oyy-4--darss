ea4064a7fac9c5c750fdb779923f16152c730943
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-09-07 18:06:37 +0500

    5-oy 4- dars

---
+++ index.js
@@ -0,0 +1,95 @@
+// 1 misol
+// let rer = 'Salom Xayr Bolalar';
+// let upperCount = 0;
+// let lowerCount = 0;
+
+// function name(arg) {
+//     for (const iterator of arg) {
+//         if (iterator >= 'A' && iterator <= 'Z') {
+//             upperCount++;
+//         } else if (iterator >= 'a' && iterator <= 'z') {
+//             lowerCount++;
+//         }
+//     }
+//     return {
+//         upperCaseCount: upperCount,
+//         lowerCaseCount: lowerCount
+//     };
+// }
+// const natija = name(rer);
+// console.log(upperCount);
+// console.log(lowerCount);
+
+// 2 miso
+//  const Palindrom = (str) => {
+//         for (const i of str) {
+//             if (i == "radar"  i == "madam"  i == "refer") {
+//                 res.push(i);
+//             }
+//         }
+//         return res.join(' ');
+//     }
+//     let test = ["radar", "ovoz", "madam", "auto", "refer"];
+//     let res = [];
+//     console.log(Palindrom(test));
+// 3 misol
+// function name(arg) {
+//     let counter = 0;
+//     for (const iterator of arg) {
+//         if (iterator == son) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+// let son = 4;
+// let test = [1,2,3,4,3,6];
+// console.log(name(test));
+
+// 4 misol
+
+// 5 misol
+
+// let res = ;
+// function arr(n) {
+//     if(n <=1){
+//     return n
+//     }
+//     return arr(n -1) + arr(n -2);
+// }
+// console.log(arr(res));
+
+// 6 misol
+
+// function name(arg) {
+//   let arr1 = arg.toString();
+//   let sum = 0;
+
+//   for (const iterator of arr1) {
+//     sum += Number(iterator);
+//   }
+
+//   return sum;
+// }
+// const res = 1234;
+// const natija = name(res);
+
+// console.log(natija);
+
+// 7 misol
+// 8 misol
+
+// function name(arg) {
+//   let arr = arg.split(" ");
+
+//   let res = arr.map((soz) => soz.split("").reverse().join(""));
+
+//   return res.join(" ");
+// }
+
+// let arg = "Salom Xayr Bolalar";
+// let natija = name(arg);
+
+// console.log(natija);
+
+
